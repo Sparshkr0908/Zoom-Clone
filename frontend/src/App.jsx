befeabf009885a12a4de633590e13452a77a7319
Sparshkr0908 <sparshkumar0410@gmail.com>
2026-09-16 10:20:19 +0530

    Adding Landing Page and Begin of Our Frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,17 @@
 import React from "react";
+import {Route, BrowserRouter as Router, Routes} from "react-router-dom";
+import "./App.css";
+import LandingPage from "./pages/landing";"./pages/landing.jsx"
 
 function App(){
     return(
-        <></>
+        <div className="App">
+            <Router>
+                <Routes>
+                    <Route  path = '/' element = {<LandingPage/>}/>
+                </Routes>
+            </Router>
+        </div>
     );
 }
 
