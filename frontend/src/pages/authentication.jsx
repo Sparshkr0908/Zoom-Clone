9459d524ebf44a1a900ff8acbf7c021a2fcca620
Sparshkr0908 <sparshkumar0410@gmail.com>
2026-09-21 11:11:05 +0530

    Update auntentication and app according to AuthContext

--- frontend/src/pages/authentication.jsx
+++ frontend/src/pages/authentication.jsx
@@ -9,6 +9,7 @@ import Grid from "@mui/material/Grid";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Snackbar from "@mui/material/Snackbar";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { AuthContext } from "../contexts/AuthContext";
 
 const defaultTheme = createTheme();
 
@@ -21,6 +22,33 @@ export default function Authentication() {
 
     const [formState, setFormState] = React.useState(0);
     const [open, setOpen] = React.useState(false);
+
+    let handleAuth = async () => {
+        try {
+            if (formState === 0) {
+
+                let result = await handleLogin(username, password)
+
+
+            }
+            if (formState === 1) {
+                let result = await handleRegister(name, username, password);
+                console.log(result);
+                setUsername("");
+                setMessage(result);
+                setOpen(true);
+                setError("")
+                setFormState(0)
+                setPassword("")
+            }
+        } catch (err) {
+
+            console.log(err);
+            let message = (err.response.data.message);
+            setError(message);
+        }
+    }
+
 
     return (
         <ThemeProvider theme={defaultTheme}>
@@ -169,10 +197,9 @@ export default function Authentication() {
                                     mt: 3,
                                     mb: 2,
                                 }}
+                                onClick={handleAuth}
                             >
-                                {formState === 0
-                                    ? "Login"
-                                    : "Register"}
+                                {formState === 0 ? "Login" : "Register"}
                             </Button>
                         </Box>
                     </Box>
