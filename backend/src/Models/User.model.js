e8175fd690f81da7d670e2657cc5185c249303eb
Sparshkr0908 <sparshkumar0410@gmail.com>
2026-09-25 14:38:05 +0530

    here we add token expiry

--- backend/src/Models/User.model.js
+++ backend/src/Models/User.model.js
@@ -5,7 +5,8 @@ const userSchema = new Schema(
         name: {type: String, required: true},
         username: {type: String, required: true, unique: true},
         password: {type: String, required: true},
-        token: {type: String}
+        token: {type: String},
+        tokenExpiresAt: {type: Date}
     }
 )
 const User = mongoose.model("User", userSchema);
