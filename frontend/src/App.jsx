9bb13741f60275e358a82f28844105df169ab86e
Sparshkr0908 <sparshkumar0410@gmail.com>
2026-09-25 09:59:17 +0530

    add history component to check the history of meeting

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,7 +17,7 @@ function App(){
                         <Route  path = '/' element = {<LandingPage/>}/>
                         <Route path="/auth" element = {<Authentication/>}/>
                         <Route path='/home's element={<HomeComponent />} />
-                        
+                        <Route path='/history' element={<History />} />
                         <Route path = "/:url" element = {<VideoMeeting/>}/>
                     </Routes>
                 </AuthProvider>
