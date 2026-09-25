c1bed26f39ce1135033fd77a69ec7dba2453bf1e
Sparshkr0908 <sparshkumar0410@gmail.com>
2026-09-25 14:39:00 +0530

    add routes to verify token

--- backend/src/Routes/User.route.js
+++ backend/src/Routes/User.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import {login, register, addToHistory, getUserHistory} from "../Controllers/user.controller.js";
+import {login, register, addToHistory, getUserHistory, verifyToken} from "../Controllers/user.controller.js";
 
 
 const router = Router();
@@ -8,5 +8,6 @@ router.route("/login").post(login)
 router.route("/register").post(register)
 router.route("/add_to_activity").post(addToHistory)
 router.route("/get_all_activity").get(getUserHistory)
+router.route("/verify").get(verifyToken) 
 
 export default router;
