87a57e4557bc0f4d85e378fedd19376d97f3f033
Sparshkr0908 <sparshkumar0410@gmail.com>
2026-09-25 14:42:35 +0530

    update disconnect callback function

--- backend/src/Controllers/SocketManager.js
+++ backend/src/Controllers/SocketManager.js
@@ -1,4 +1,4 @@
-import {Server, Socket} from "socket.io"
+import {Server} from "socket.io"
 
 let connections = {}
 let messages = {}
@@ -8,9 +8,7 @@ export const connectToSocket = (server) =>{
     const io = new Server(server, {
         cors: {
             origin: "*",
-            methods: ["GET", "POST"],
-            allowedHeaders: ["*"],
-            credentials: true
+            methods: ["GET", "POST"]
         }
     });
 
@@ -57,8 +55,6 @@ export const connectToSocket = (server) =>{
                 }
 
                 messages[matchingRoom].push({'sender':sender, "data": data, "socket-id-sender":socket.id})
-                console.log("message", matchingRoom, ":", sender, data)
-
                 connections[matchingRoom].forEach((elem)=>{
                     io.to(elem).emit("chat-message", data, sender, socket.id)
                 })
@@ -67,28 +63,24 @@ export const connectToSocket = (server) =>{
 
         socket.on("disconnect", ()=>{
             var diffTime = Math.abs(timeOnline[socket.id] - new Date())
-
+            //console.log(`Socket ${socket.id} was online for ${Math.round(diffTime / 1000)} seconds`)
             var key
-
             for(const [k,v] of JSON.parse(JSON.stringify(Object.entries(connections)))){
                 for(let a = 0; a < v.length; ++a){
                     if(v[a] === socket.id){
                         key = k
-
                         for(let a = 0; a<connections[key].length; ++a){
                             io.to(connections[key][a]).emit('user-left', socket.id)
                         }
-
                         var index = connections[key].indexOf(socket.id)
-
                         connections[key].splice(index, 1)
-
                         if(connections[key].length === 0){
                             delete connections[key]
                         }
                     }
                 }
             }
+            delete timeOnline[socket.id]
         })
     })
 
